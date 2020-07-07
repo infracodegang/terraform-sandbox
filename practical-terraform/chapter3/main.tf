7c284af16df1f5f0b32a32ca0f57095b6577b45e
perforb <dev.perfumed.garden@gmail.com>
2020-07-07 20:42:35 +0900

    first commit

---
+++ practical-terraform/chapter3/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  profile = "default"
+  region  = "ap-northeast-1"
+}
+
+module "web_server" {
+  source        = "./http_server"
+  instance_type = "t3.micro"
+}
+
+output "public_dns" {
+  value = module.web_server.public_dns
+}
