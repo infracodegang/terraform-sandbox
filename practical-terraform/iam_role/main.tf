bd1015cb26f0e605e7ddd4097407fee31b16fead
perforb <dev.perfumed.garden@gmail.com>
2020-07-08 18:35:40 +0900

    Add a new module "iam_role"

---
+++ practical-terraform/iam_role/main.tf
@@ -0,0 +1,49 @@
+variable "name" {}
+variable "policy" {}
+variable "identifier" {}
+
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:DescribeRegions"] # リージョン一覧を取得する
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = [var.identifier]
+    }
+  }
+}
+
+resource "aws_iam_role" "default" {
+  name               = var.name
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_policy" "default" {
+  name   = var.name
+  policy = var.policy
+}
+
+resource "aws_iam_role_policy_attachment" "default" {
+  role       = aws_iam_role.default.name
+  policy_arn = aws_iam_policy.default.arn
+}
+
+output "iam_role_arn" {
+  value = aws_iam_role.default.arn
+}
+
+output "iam_role_name" {
+  value = aws_iam_role.default.name
+}
+
+output "describe_regions_json" {
+  value = data.aws_iam_policy_document.allow_describe_regions.json
+}
--- practical-terraform/main.tf
+++ practical-terraform/main.tf
@@ -8,6 +8,13 @@ module "web_server" {
   instance_type = "t3.micro"
 }
 
+module "describe_regions_for_ec2" {
+  source     = "./iam_role"
+  name       = "describe-regions-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy     = module.describe_regions_for_ec2.describe_regions_json
+}
+
 output "public_dns" {
   value = module.web_server.public_dns
 }
