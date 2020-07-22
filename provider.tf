b8880a992d352b5480ea342509f85ca79fb3c36a
perforb <dev.perfumed.garden@gmail.com>
2020-07-22 17:04:49 +0900

    Fixed versios

--- provider.tf
+++ provider.tf
@@ -1,8 +1,14 @@
+terraform {
+  required_version = "0.12.28"
+}
+
 provider "aws" {
   profile = "default"
   region  = "ap-northeast-1"
+  version = "2.69.0"
 }
 
 provider "github" {
   organization = "refurbishedcodes"
+  version      = "2.9.2"
 }
