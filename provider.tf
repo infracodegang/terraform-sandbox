af532b899e4e84bb46a04c5ff73d1887a0ce97a1
perforb <dev.perfumed.garden@gmail.com>
2020-07-29 12:05:37 +0900

    Added required versions

--- provider.tf
+++ provider.tf
@@ -1,7 +1,3 @@
-terraform {
-  required_version = "0.12.28"
-}
-
 provider "aws" {
   profile = "default"
   region  = "ap-northeast-1"
--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,11 @@
 terraform {
+  required_version = ">= 0.12.28"
+
+  required_providers {
+    aws    = ">= 2.69.0"
+    github = ">= 2.9.2"
+  }
+
   backend "remote" {
     organization = "infracodegang"
 
