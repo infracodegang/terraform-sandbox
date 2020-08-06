bc152bcfc6ddc713bf0534ce82a89ac798eee544
perforb <dev.perfumed.garden@gmail.com>
2020-08-06 19:12:31 +0900

    Added a script file for creating the remote state bucket

---
+++ two-tier/environments/dev/create_backend_bucket.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+cd $(dirname "$0") || exit
+
+read -p "Remote State Bucket [unique bucket name]: " BUCKET_NAME
+if [[ -z "${BUCKET_NAME}" ]]; then
+  echo "Bucket name must not be empty!" 1>&2
+  exit 1
+fi
+
+REGION="ap-northeast-1"
+
+## Create a remote-state bucket
+read -p "create-bucket? [y/N]: " yn
+case "${yn}" in
+  [yY]*) aws s3api create-bucket --bucket "${BUCKET_NAME}" --create-bucket-configuration LocationConstraint="${REGION}" ;;
+  *) ;;
+esac
+
+## Versioning
+read -p "put-bucket-versioning? [y/N]: " yn
+case "${yn}" in
+  [yY]*) aws s3api put-bucket-versioning --bucket "${BUCKET_NAME}" --versioning-configuration Status=Enabled ;;
+  *) ;;
+esac
+
+## Encryption
+read -p "put-bucket-encryption? [y/N]: " yn
+case "${yn}" in
+  [yY]*) aws s3api put-bucket-encryption --bucket "${BUCKET_NAME}" --server-side-encryption-configuration '{"Rules":[{"ApplyServerSideEncryptionByDefault":{"SSEAlgorithm":"AES256"}}]}'
+  ;;
+  *) ;;
+esac
+
+## Public acccess block
+read -p "put-public-access-block? [y/N]: " yn
+case "${yn}" in
+  [yY]*) aws s3api put-public-access-block --bucket "${BUCKET_NAME}" --public-access-block-configuration '{"BlockPublicAcls":true,"IgnorePublicAcls":true,"BlockPublicPolicy":true,"RestrictPublicBuckets":true}'
+  ;;
+  *) ;;
+esac
