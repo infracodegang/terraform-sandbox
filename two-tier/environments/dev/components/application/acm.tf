46cbf0eb6c31741cc59c037ab6e06c5eb4b7b97c
perforb <dev.perfumed.garden@gmail.com>
2020-08-08 00:56:39 +0900

    Refactored

---
+++ two-tier/environments/dev/components/application/acm.tf
@@ -0,0 +1,15 @@
+resource "aws_acm_certificate" "acm_cert" {
+  domain_name               = var.domain_name
+  subject_alternative_names = []
+  validation_method         = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate_validation
+resource "aws_acm_certificate_validation" "acm_cert" {
+  certificate_arn         = aws_acm_certificate.acm_cert.arn
+  validation_record_fqdns = [aws_route53_record.root_validation.fqdn]
+}
--- two-tier/environments/dev/components/application/route53.tf
+++ two-tier/environments/dev/components/application/route53.tf
@@ -14,16 +14,6 @@ resource "aws_route53_record" "root" {
   }
 }
 
-resource "aws_acm_certificate" "acm_cert" {
-  domain_name               = var.domain_name
-  subject_alternative_names = []
-  validation_method         = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate#referencing-domain_validation_options-with-for_each-based-resources
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/guides/version-3-upgrade
 resource "aws_route53_record" "root_validation" {
@@ -32,10 +22,4 @@ resource "aws_route53_record" "root_validation" {
   records = [tolist(aws_acm_certificate.acm_cert.domain_validation_options)[0].resource_record_value]
   zone_id = data.aws_route53_zone.root.id
   ttl     = 60
-}
-
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate_validation
-resource "aws_acm_certificate_validation" "acm_cert" {
-  certificate_arn         = aws_acm_certificate.acm_cert.arn
-  validation_record_fqdns = [aws_route53_record.root_validation.fqdn]
 }
