952c331a8ac881483eccbc9bfbf8d2526a65d99a
perforb <dev.perfumed.garden@gmail.com>
2020-08-12 22:13:30 +0900

    Clean up some codes

--- two-tier/environments/dev/components/application/logging.tf
+++ two-tier/environments/dev/components/application/logging.tf
@@ -1,4 +1,4 @@
 resource "aws_cloudwatch_log_group" "for_ecs_api" {
   name              = "/ecs/api"
-  retention_in_days = var.cloudwatch_log_group_for_ecs_api_retention_in_days
+  retention_in_days = 30
 }
