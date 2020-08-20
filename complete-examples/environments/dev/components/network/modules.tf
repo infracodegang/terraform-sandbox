d9b3a05895b22a34bff16d97c5bf0c6af8d422d6
perforb <dev.perfumed.garden@gmail.com>
2020-08-20 19:18:33 +0900

    Add private link definitions

---
+++ complete-examples/environments/dev/components/network/modules.tf
@@ -0,0 +1,10 @@
+# https://aws.amazon.com/jp/blogs/news/aws-fargate-launches-platform-version-1-4/?sc_channel=sm&sc_campaign=AWS_Blog,launch_&sc_publisher=TWITTER&sc_country=Japan&sc_geo=JAPAN&sc_outcome=awareness&trkCampaign=c&trk=aws-fargate-launches-platform-version-1-4_TWITTER&sc_content=aws-fargate-launches-platform-version-1-4&sc_category=AWS+Fargate&linkId=87467865
+# https://blog.fakiyer.com/entry/2020/05/11/184759
+# https://dev.classmethod.jp/articles/privatesubnet_ecs/
+module "ecs_private_link_sg" {
+  source      = "../../../../modules/security_group"
+  name        = "ecs-private-link-sg"
+  vpc_id      = aws_vpc.main.id
+  port        = 443
+  cidr_blocks = [var.vpc_cidr_block]
+}
--- complete-examples/environments/dev/components/network/vpc.tf
+++ complete-examples/environments/dev/components/network/vpc.tf
@@ -18,3 +18,73 @@ resource "aws_internet_gateway" "main" {
     Environment = var.env
   }
 }
+
+# https://dev.classmethod.jp/articles/fargate_pv14_vpc_endpoint/
+# https://dev.classmethod.jp/articles/privatesubnet_ecs/
+resource "aws_vpc_endpoint" "logs" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.ap-northeast-1.logs"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [
+    aws_subnet.private_1.id,
+    aws_subnet.private_2.id,
+  ]
+  security_group_ids  = [module.ecs_private_link_sg.security_group_id]
+  private_dns_enabled = true
+
+  tags = {
+    Name        = "vpc_endpoint_logs"
+    Environment = var.env
+  }
+}
+
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.ap-northeast-1.ecr.dkr"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [
+    aws_subnet.private_1.id,
+    aws_subnet.private_2.id,
+  ]
+  security_group_ids  = [module.ecs_private_link_sg.security_group_id]
+  private_dns_enabled = true
+
+  tags = {
+    Name        = "vpc_endpoint_ecr_dkr"
+    Environment = var.env
+  }
+}
+
+resource "aws_vpc_endpoint" "ecr_api" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.ap-northeast-1.ecr.api"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [
+    aws_subnet.private_1.id,
+    aws_subnet.private_2.id,
+  ]
+  security_group_ids  = [module.ecs_private_link_sg.security_group_id]
+  private_dns_enabled = true
+
+  tags = {
+    Name        = "vpc_endpoint_ecr_api"
+    Environment = var.env
+  }
+}
+
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.ap-northeast-1.ssm"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [
+    aws_subnet.private_1.id,
+    aws_subnet.private_2.id,
+  ]
+  security_group_ids  = [module.ecs_private_link_sg.security_group_id]
+  private_dns_enabled = true
+
+  tags = {
+    Name        = "vpc_endpoint_ssm"
+    Environment = var.env
+  }
+}
