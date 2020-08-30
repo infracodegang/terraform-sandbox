c84a490d436d7d6198e4555cb26a21e913eef675
perforb <dev.perfumed.garden@gmail.com>
2020-08-30 20:39:42 +0900

    Modified container definitions

--- complete-examples/environments/dev/components/application/cloudwatch_logs.tf
+++ complete-examples/environments/dev/components/application/cloudwatch_logs.tf
@@ -1,8 +1,3 @@
-resource "aws_cloudwatch_log_group" "for_ecs_web" {
-  name              = "/ecs/web"
-  retention_in_days = 30
-}
-
 resource "aws_cloudwatch_log_group" "for_ecs_api" {
   name              = "/ecs/api"
   retention_in_days = 30
