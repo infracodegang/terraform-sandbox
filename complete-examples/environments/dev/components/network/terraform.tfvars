43d169870f707c31bc9a6a300da2c0b9b1c49a9b
perforb <dev.perfumed.garden@gmail.com>
2020-09-05 10:32:57 +0900

    Refactored

--- complete-examples/environments/dev/components/network/terraform.tfvars
+++ complete-examples/environments/dev/components/network/terraform.tfvars
@@ -2,3 +2,12 @@
 
 profile = "default"
 env = "dev"
+vpc_cidr_block = "10.0.0.0/16"
+public_subnet_1_cidr_block = "10.0.1.0/24"
+public_subnet_2_cidr_block = "10.0.2.0/24"
+private_subnet_1_cidr_block = "10.0.65.0/24"
+private_subnet_2_cidr_block = "10.0.66.0/24"
+public_subnet_1_az = "ap-northeast-1a"
+public_subnet_2_az = "ap-northeast-1c"
+private_subnet_1_az = "ap-northeast-1a"
+private_subnet_2_az = "ap-northeast-1c"
--- complete-examples/environments/dev/components/network/variables.tf
+++ complete-examples/environments/dev/components/network/variables.tf
@@ -16,54 +16,45 @@ variable "region" {
 
 variable "vpc_cidr_block" {
   type        = string
-  default     = "10.0.0.0/16"
   description = ""
 }
 
 variable "public_subnet_1_cidr_block" {
   type        = string
-  default     = "10.0.1.0/24"
   description = ""
 }
 
 variable "public_subnet_2_cidr_block" {
   type        = string
-  default     = "10.0.2.0/24"
   description = ""
 }
 
 variable "private_subnet_1_cidr_block" {
   type        = string
-  default     = "10.0.65.0/24"
   description = ""
 }
 
 variable "private_subnet_2_cidr_block" {
   type        = string
-  default     = "10.0.66.0/24"
   description = ""
 }
 
 variable "public_subnet_1_az" {
   type        = string
-  default     = "ap-northeast-1a"
   description = ""
 }
 
 variable "public_subnet_2_az" {
   type        = string
-  default     = "ap-northeast-1c"
   description = ""
 }
 
 variable "private_subnet_1_az" {
   type        = string
-  default     = "ap-northeast-1a"
   description = ""
 }
 
 variable "private_subnet_2_az" {
   type        = string
-  default     = "ap-northeast-1c"
   description = ""
 }
